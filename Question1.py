bdb79bedb8c01be57250715b3e286c03990a0d82
ericjiang107 <eric.jiang@student.fairfield.edu>
2021-06-21 19:09:08 -0400

    Update With Answers

--- Question1.py
+++ Question1.py
@@ -1,2 +1,10 @@
-x = 1 
-print(x)
+"""Write a function to print hello_USERNAME! USERNAME is the input of the function. The first line of the code has been defined as below.""" 
+# looking for def function not user input
+
+def hello_name(user_name): 
+    print("hello_" + user_name.title() + "!")
+hello_name("eric")
+
+# for user input
+user_name = input("Enter Username ")
+print("hello_" + user_name.title() + "!")
