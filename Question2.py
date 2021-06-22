73ee7d31be7e3fc22a4a53d0e55225cb8496db61
ericjiang107 <eric.jiang@student.fairfield.edu>
2021-06-21 20:03:54 -0400

    Answer to Question

--- Question2.py
+++ Question2.py
@@ -1,2 +1,19 @@
-x = 1+1
-print(x)
+"""Print first 100 odd numbers in Python"""
+
+#using counter to compare to said value. The counter counts how many times the while loop is performed. 
+#using while
+
+n = 1
+counter = 0 
+while counter < 100:
+    n += 2
+    print(n)
+    counter += 1
+
+
+#using for loop 
+#y is acting as the counter in this case
+n = 1
+for y in range(1, 101):
+    n += 2 
+    print(n)
