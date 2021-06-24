919a0748ab997ae7bc52efba2b568488f35d5bf1
ericjiang107 <eric.jiang@student.fairfield.edu>
2021-06-24 17:32:12 -0400

    Updated Answer

---
+++ Question3.py
@@ -0,0 +1,12 @@
+"""Write a Python function, max_num_in_list to return the max number of a given list. The first line of the code has been defined as below.""" 
+
+
+def max_num_in_list(a_list):
+    maxNum = 0
+    for currNum in a_list:
+        if currNum > maxNum:
+            maxNum = currNum
+    return maxNum
+
+
+max_num_in_list([1, 2, 3, 4, 5, 10, 1, 2, 3, 7])
